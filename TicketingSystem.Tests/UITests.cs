6dbdcb4f1f785346a44e0073e16d7d3b7ed4d519
george cox <georgecox67@hotmail.co.uk>
2020-02-21 15:07:35 +0000

    Made new Project to set up testing

---
+++ TicketingSystem.Tests/UITests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TicketingSystem;
+
+namespace TicketingSystem.Tests
+{
+    public class UITests
+    {
+        public void ChangeDisplay_InitialMenu()
+        {
+            // Arrange 
+
+
+            // Act
+
+            // Assert
+        }
+    }
+}
